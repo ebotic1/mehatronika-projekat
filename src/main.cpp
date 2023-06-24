26af775b44cde115973e553bc82bbe52c0ea6efc
ebotic1 <ebotic1@etf.unsa.ba>
2023-06-24 18:38:00 +0200

    diff

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,39 @@ const int naponPin = 39;
 Servo motorSpin;
 Servo motorRoll;
 
+double spin = 65;
+double roll = 40;
+
 void setup(){
     Serial.begin(115200);
      motorSpin.attach(4);
      motorRoll.attach(2);
 
-    motorSpin.write(65);
-    motorRoll.write(40);
+    motorSpin.write(spin);
+    motorRoll.write(roll);
 
 }
 
 void loop(){
   
-  int napon = analogRead(naponPin);
   
-  Serial.println(napon);
+  Serial.println(diferencijal(&motorSpin, 5));
 
-
-  delay(1001);
+  delay(1500);
 
 }
 
 
+double diferencijal(motorSpin *s, double step){
+
+  int napon = analogRead(naponPin);
+
+  s->write(spin + step);
+  delay(300);
+  double rezultat = (analogRead(naponPin) - napon)/step;
+
+  s->write(spin - step);
+
+  return rezultat;
+
+}
