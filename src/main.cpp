88a40dec618a9e6582954f51af30e9c20099d40b
eb <ebotic1@etf.unsa.ba>
2023-06-24 17:34:01 +0200

    prvi commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+const int naponPin = 39;
+Servo motorSpin;
+Servo motorRoll;
+
+void setup(){
+    Serial.begin(115200);
+     motorSpin.attach(4);
+     motorRoll.attach(2);
+
+    motorSpin.write(65);
+    motorRoll.write(40);
+
+}
+
+void loop(){
+  
+  int napon = analogRead(naponPin);
+  
+  Serial.println(napon);
+
+  delay(1000);
+
+}
+
+
