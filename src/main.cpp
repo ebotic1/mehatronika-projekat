ed9b9d813de144a06b8cb33df888469f98d6466c
ebotic1 <ebotic1@etf.unsa.ba>
2023-06-24 20:31:54 +0200

    motor

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,67 @@ const int naponPin = 39;
 Servo motorSpin;
 Servo motorRoll;
 
+double spin = 55;
+double roll = 40;
+
+void advance(Servo *s, double poz)
+{
+  /*
+  int znak = (s->read() < poz) ? 1 : -1;
+  if (znak == 1)
+  {
+    for (int pos = s->read(); pos <= poz; pos += 1)
+    {
+      s->write(pos); // Move to the current position
+      delay(500);    // Delay for smoother movement
+    }
+  }
+  else
+    {
+      for (int pos = s->read(); pos >= poz; pos -= 1)
+      {
+        s->write(pos); // Move to the current position
+        delay(500);    // Delay for smoother movement
+      }
+    }
+*/
+    s->write(poz);
+  
+}
+
+double diferencijal(Servo * s, double step) {
+  int napon = analogRead(naponPin);
+
+  advance(s, spin + step);
+    
+
+  delay(1000);
+  double rezultat = (analogRead(naponPin) - napon)/step;
+  delay(1000);
+
+  advance(s, spin-step);
+
+  return rezultat;
+}
+
 void setup(){
     Serial.begin(115200);
      motorSpin.attach(4);
      motorRoll.attach(2);
 
-    motorSpin.write(65);
-    motorRoll.write(40);
+    advance(&motorSpin, spin);
+    advance(&motorRoll, roll);
 
 }
 
 void loop(){
   
-  int napon = analogRead(naponPin);
   
-  Serial.println(napon);
+  Serial.println(diferencijal(&motorSpin, 10));
 
 
 
-  delay(1001);
+  delay(8000);
 
 }
-
 
