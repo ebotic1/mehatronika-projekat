4a656bc56af2c4c99b3e6cfb5bd1793e46a3ee5a
ebotic1 <ebotic1@etf.unsa.ba>
2023-06-25 23:53:07 +0200

    radi

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,53 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-const int naponPin = 4;
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <vector>
+
+const char *dajStranicu(std::vector<int> &vrijeme, std::vector<int> &napon);
+
+const char* ssid = "PROMES-W4";
+const char* password = "";
+
+std::vector<int> napon, vrijeme;
+
+AsyncWebServer server(80); // create AsyncWebServer object on port 80
+
+
+const int naponPin = 39;
 Servo motorSpin;
 Servo motorRoll;
 
-
-bool startup = true;
+int lastPositioning = 0;
+const int positionTimeout = 240;
 
 void advance(Servo *s, int poz)
 {
-  poz = poz - (poz / 360) * 360;
-  if(poz < 0) poz+=360;
+  if(poz > 180 || poz < 0){
+    Serial.print("value not allowed: ");
+    Serial.println(poz);
+    poz = (poz > 180) ? 180 : 0;
+  }
+
   double trenuta = s->read();
 
   if (trenuta < poz)
   {
-    for (int pos = trenuta; pos <= poz; pos += 1)
+    for (int pos = trenuta; pos < poz; pos += 1)
     {
       s->write(pos); // Move to the current position
       delay(10);    // Delay for smoother movement
+
     }
   }
   else
     {
-      for (int pos = trenuta; pos >= poz; pos -= 1)
+      for (int pos = trenuta; pos > poz; pos -= 1)
       {
         s->write(pos); // Move to the current position
         delay(10);    // Delay for smoother movement
+
       }
     }
 
@@ -39,11 +59,8 @@ double diferencijal(Servo * s, double step) {
   int poz = s->read();
   advance(s, poz + step);
     
-  delay(5000);
+  delay(4000);
   int novi_napon = analogRead(naponPin);
-
-  if(novi_napon == napon)
-  Serial.println("mater ti jebem");
 
   
   if(s == &motorRoll){
@@ -52,22 +69,23 @@ double diferencijal(Servo * s, double step) {
     Serial.print(" ");
     Serial.print(novi_napon);
     Serial.println(" info end");
-    novi_napon = 40;
   }
 
   delay(500);
 
-  advance(s, poz-step);
+  advance(s, poz);
 
 
-  return (novi_napon - napon);
+  return (novi_napon - napon)/step;
 }
 
 void nadiPoziciju(int preciznost){
 
-  int sp = diferencijal(&motorSpin, preciznost);
-  delay(3000);
-  int rol = diferencijal(&motorRoll, preciznost);
+  const int scale = 10;
+
+  int sp = diferencijal(&motorSpin, preciznost) * scale;
+  delay(2000);
+  int rol = diferencijal(&motorRoll, preciznost) * scale;
 
   double duzina = sqrt(sp*sp + rol*rol);
 
@@ -75,10 +93,9 @@ void nadiPoziciju(int preciznost){
 
   if(duzina == 0)
     return;
-  if(sp > 5)
+
   advance(&motorSpin, motorSpin.read() + preciznost * (sp/duzina) );
 
-  if(rol > 5)
   advance(&motorRoll, motorRoll.read() + preciznost * (rol/duzina) );
 
 
@@ -94,30 +111,52 @@ void setup(){
 
 
 
-    motorSpin.write(180);
-    motorRoll.write(180);
+    motorSpin.write(0);
+    motorRoll.write(0);
+
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println(WiFi.localIP());
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/html", dajStranicu(vrijeme, napon));
+    });
+
+    server.begin();
+
+    //nadiPoziciju(180);
+    //nadiPoziciju(90);
 
 }
 
+int cnt_old = 0;
 
 void loop(){
 
-//Serial.println(analogRead(naponPin));
-//advance(&motorRoll, -10);
-//delay(900);
-  //return;
+ // return;
 
+int cnt = (millis() / 1000) / positionTimeout;
+
+if(cnt > cnt_old){
+  cnt_old = cnt;
   Serial.println("pocetak");
-
-  for(int i = 170; i>0; i=i/2){
+  for(int i = 40; i>0; i=i/2){
     nadiPoziciju(i);
     delay(2000);
+  }
+  Serial.println("kraj");
 }
 
 
-  Serial.println("kraj");
+  vrijeme.push_back(millis() / 1000);
+  napon.push_back(analogRead(naponPin));
 
-  delay(100000);
 
+  delay(10000);
 }
 
---
+++ src/stranica.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <string>
+
+const char *dajStranicu(std::vector<int> &vrijeme, std::vector<int> &napon){
+    const char *prvi_dio = "<!DOCTYPE html>\n"
+                        "<html>\n"
+                        "<head>\n"
+                        "    <title>Function Graph</title>\n"
+                        "    <script src=\"https://cdn.jsdelivr.net/npm/chart.js\"></script>\n"
+                        "</head>\n"
+                        "<body>\n"
+                        "    <canvas id=\"myChart\"></canvas>\n"
+                        "    <script>\n"
+                        "        function myFunction(x) {\n"
+                        "            return Math.sin(x);\n"
+                        "        }\n"
+                        "        var data = [];\n";
+
+    std::string s(prvi_dio);
+
+    for(int i = 0; i<vrijeme.size(); ++i){
+        s += "data.push({ x:";
+        s += std::to_string(vrijeme[i]);
+        s+= ", y: ";
+        s+= std::to_string(2 * napon[i] * 3.3/4095);
+        s+= " });\n";
+
+    }
+
+    s += "        var ctx = document.getElementById('myChart').getContext('2d');\n"
+                        "        var chart = new Chart(ctx, {\n"
+                        "            type: 'line',\n"
+                        "            data: {\n"
+                        "                datasets: [{\n"
+                        "                    label: 'Function Graph',\n"
+                        "                    data: data,\n"
+                        "                    backgroundColor: 'rgba(75, 192, 192, 0.2)',\n"
+                        "                    borderColor: 'rgba(75, 192, 192, 1)',\n"
+                        "                    borderWidth: 1\n"
+                        "                }]\n"
+                        "            },\n"
+                        "            options: {\n"
+                        "                scales: {\n"
+                        "                    x: {\n"
+                        "                        type: 'linear',\n"
+                        "                        position: 'bottom'\n"
+                        "                    }\n"
+                        "                }\n"
+                        "            }\n"
+                        "        });\n"
+                        "    </script>\n"
+                        "</body>\n"
+                        "</html>";
+
+                        return s.c_str();
+}
